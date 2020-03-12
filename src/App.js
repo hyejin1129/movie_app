5096e650ab9cc4fbaa2f3744dc3ca5c74bfac561
김혜진 <hy-kim@bluedigm.com>
2020-03-12 11:48:28 +0900

    Food Component Bye~

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ProtoTypes from 'prop-types';
+
+function Food({name, image, rating}){
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={image} alt={name}/>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: ProtoTypes.string.isRequired,
+  image: ProtoTypes.string.isRequired,
+  rating: ProtoTypes.number.isRequired
+}
+
+const foodIlike = [
+  {id: 1, 
+    name: "kimchi", 
+    image: "https://blogfiles.pstatic.net/20120821_46/daesoop301_1345510791149pHwN8_JPEG/%C6%F7%B1%E2%B1%E8%C4%A1.jpg",
+    rating: 5.5},
+  {id: 2, 
+    name: "samgyobsal", 
+    image: "http://blogfiles.naver.net/20140303_202/samsubk_1393819190486AofzR_JPEG/%BB%EF%B0%E3%BB%EC3.jpg",
+    rating: 2.3},
+  {id: 3, 
+    name: "bibimbap", 
+    image: "http://blogfiles.naver.net/20150226_125/gvogong1388_1424916521020HsweG_JPEG/%C0%FC%C1%D6%BF%A1_%BF%C0%B8%E9_%B2%C0_%C0%CC%B0%C7_%B8%D4%BE%EE%BE%DF%C7%D1%B4%D9BEST_%C3%DF%C3%B5%C7%D8%B5%E5%B7%C1%BF%E41.jpg",
+    rating: 3.5}
+];
+
+function FoodMap(dish){
+  return <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1>Hello!</h1>
+    {foodIlike.map(FoodMap)}
+  </div>
   );
 }
 
